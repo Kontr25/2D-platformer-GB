e3ac864c812c63e3290a492f00fb10fcfad82e5a
Artyom <buraev22@yandex.ru>
2022-09-30 20:02:36 +0400

    Lesson5

--- Assets/_Scripts/Enemy/EnemyView.cs
+++ Assets/_Scripts/Enemy/EnemyView.cs
@@ -4,6 +4,9 @@ namespace _Scripts.Enemy
     public class EnemyView: MonoBehaviour
     {
         [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Rigidbody2D _rigidbody;
+
+        public Rigidbody2D Rigidbody => _rigidbody;
 
         public SpriteRenderer SpriteRenderer
         {
--- Assets/_Scripts/EntryPoint.cs
+++ Assets/_Scripts/EntryPoint.cs
@@ -5,6 +5,7 @@ using _Scripts.Cannon;
 using _Scripts.Character;
 using _Scripts.Enemy;
 using _Scripts.Environment;
+using Pathfinding;
 using UnityEngine;
 
 public class EntryPoint : MonoBehaviour
@@ -14,21 +15,33 @@ public class EntryPoint : MonoBehaviour
     [SerializeField] private CharacterView _characterView;
     [SerializeField] private EnemyView _enemyView;
     [SerializeField] private ObstacleView _fireballView;
+    [SerializeField] private EnemyView _batView;
     [SerializeField] private SpriteAnimationsConfig _CharacterSpriteAnimationConfig;
     [SerializeField] private SpriteAnimationsConfig _EnemySpriteAnimationConfig;
     [SerializeField] private SpriteAnimationsConfig _FireBallSpriteAnimationConfig;
+    [SerializeField] private SpriteAnimationsConfig _BatSpriteAnimationConfig;
     [SerializeField] private CannonView _cannonView;
     [SerializeField] private List<BulletView> _bullets;
+    
+    [Header("Protector AI")]
+    [SerializeField] private AIDestinationSetter _protectorAIDestinationSetter;
+    [SerializeField] private AIPatrolPath _protectorAIPatrolPath;
+    [SerializeField] private LevelObjectTrigger _protectedZoneTrigger;
+    [SerializeField] private Transform[] _protectorWaypoints;
 
     private ContactsPoller _contactsPoller;
     private ParalaxManager _paralaxManager;
     private SpriteAnimator _characterSpriteAnimator;
     private SpriteAnimator _wolfSpriteAnimator;
     private SpriteAnimator _fireballSpriteAnimator;
+    private SpriteAnimator _batSpriteAnimator;
     private MainHeroWalker _mainHeroWalker;
     private MainHeroPhysicWalker _mainHeroPhysicWalker;
     private AimingMuzzle _aimingMuzzle;
     private BulletsEmitter _bulletsEmitter;
+    private SimplePatrolAI _simplePatrolAI;
+    private ProtectorAI _protectorAI;
+    private ProtectedZone _protectedZone;
 
     private void Start()
     {
@@ -42,8 +55,16 @@ public class EntryPoint : MonoBehaviour
         
         _wolfSpriteAnimator = new SpriteAnimator(_EnemySpriteAnimationConfig);
         _fireballSpriteAnimator = new SpriteAnimator(_FireBallSpriteAnimationConfig);
+        _batSpriteAnimator = new SpriteAnimator(_BatSpriteAnimationConfig);
         _wolfSpriteAnimator.StartAnimation(_enemyView.SpriteRenderer, Track.idle, true, 10);
         _fireballSpriteAnimator.StartAnimation(_fireballView.SpriteRenderer, Track.idle, true, 25);
+        _batSpriteAnimator.StartAnimation(_batView.SpriteRenderer, Track.idle, true, 10);
+        
+        _protectorAI = new ProtectorAI(_characterView, new PatrolAIModel(_protectorWaypoints), _protectorAIDestinationSetter, _protectorAIPatrolPath);
+        _protectorAI.Init();
+      
+        _protectedZone = new ProtectedZone(_protectedZoneTrigger, new List<IProtector>{ _protectorAI });
+        _protectedZone.Init();
     }
 
     private void Update()
@@ -55,10 +76,17 @@ public class EntryPoint : MonoBehaviour
         _bulletsEmitter.Update();
         _wolfSpriteAnimator.Update();
         _fireballSpriteAnimator.Update();
+        _batSpriteAnimator.Update();
     }
 
     private void FixedUpdate()
     {
         _mainHeroPhysicWalker.FixedUpdate();
+    }
+
+    private void OnDestroy()
+    {
+        _protectorAI.Deinit();
+        _protectedZone.Deinit();
     }
 }
